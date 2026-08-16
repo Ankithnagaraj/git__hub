d2379eaeced382369ea88135dc340e4800dc109a
ankith <ankithn888@gmail.com>
2026-08-16 06:53:46 +0530

    new java file

---
+++ StudentGrade.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        System.out.println("\nStudent: " + name);
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } 
+        else if (marks >= 75) {
+            System.out.println("Grade: B");
+        } 
+        else if (marks >= 60) {
+            System.out.println("Grade: C");
+        } 
+        else if (marks >= 40) {
+            System.out.println("Grade: D");
+        } 
+        else {
+            System.out.println("Grade: Fail");
+        }
+
+        sc.close();
+    }
+}
